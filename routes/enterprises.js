ce4df536034f21582de6615be08d57ae52de6053
Romana <roma.kulchitskagut@gmail.com>
2025-02-11 20:33:15 +0200

    DELETE and PUT api for enterprises

--- routes/enterprises.js
+++ routes/enterprises.js
@@ -3,21 +3,51 @@ const router = express.Router();
 const Enterprise = require("../models/enterprise.model");
 
 router.get("/", async (req, res) => {
-    try {
-        const enterprises = await Enterprise.find({});
-        res.status(200).json(enterprises);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+  try {
+    const enterprises = await Enterprise.find({});
+    res.status(200).json(enterprises);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 router.post("/", async (req, res) => {
-    try {
-        const enterprise = await Enterprise.create(req.body);
-        res.status(200).json(enterprise);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
+  try {
+    const enterprise = await Enterprise.create(req.body);
+    res.status(200).json(enterprise);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const enterprise = await Enterprise.findByIdAndDelete(req.params.id);
+    if (!enterprise) {
+      return res.status(404).json({ message: "Enterprise not found" });
     }
+    res
+      .status(200)
+      .json({ message: "Enterprise deleted successfully", enterprise });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const enterprise = await Enterprise.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    if (!enterprise) {
+      return res.status(404).json({ message: "Enterprise not found" });
+    }
+    res.status(200).json(enterprise);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 module.exports = router;
